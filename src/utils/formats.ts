8f0076a7025251ecf2558bc38af7f3ddda85e7b9
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-01-14 18:46:07 -0300

    ✨ Add filename utility function to truncate long filenames

--- src/utils/formats.ts
+++ src/utils/formats.ts
@@ -1,3 +1,7 @@
 export const money = (value?:number | null):string => (value
   ? new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
   : 'R$ 0,00');
+
+export const filename = (name:string) => (name.length > 16
+    ? `${name.substring(0, 16)}...${name.substring(name.length - 4)}`
+    : name);
